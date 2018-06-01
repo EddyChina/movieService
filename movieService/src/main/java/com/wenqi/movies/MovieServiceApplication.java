d8894060627024627aa7a7b5cc370e235b85c4c5
wenqi <firelands128@gmail.com>
2018-06-01 14:46:02 -0700

    12345

--- movieService/src/main/java/com/wenqi/movies/MovieServiceApplication.java
+++ movieService/src/main/java/com/wenqi/movies/MovieServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
 @SpringBootApplication
 public class MovieServiceApplication {
     public static void main(String[] args) {
